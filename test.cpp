5b97ddac522a048bfc8ee618a09491bb477635ea
Keeevini <kevin.ni2000@gmail.com>
2021-01-14 09:41:14 -0800

    Fixes #1
    Added the unit testing for c-count

--- test.cpp
+++ test.cpp
@@ -1,4 +1,5 @@
-#include "../header/c-echo.h"
+#include "c-echo.h"
+#include "c-count.h"
 
 #include "gtest/gtest.h"
 
@@ -23,10 +24,24 @@ TEST(EchoTest, Letters) {
 	EXPECT_EQ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", echo(2, test_val));
 }
 
-
 TEST(EchoTest, MultipleBlanks) {
 	char* test_val[3]; test_val[0] = "./c-echo"; test_val[1] = ""; test_val[2] = "";
 	EXPECT_EQ(" ", echo(3, test_val));
+}
+
+TEST(CountTest, HelloWorld) {
+	std::string test_str = "hello world";
+	EXPECT_EQ(2, count(test_str));
+}
+
+TEST(CountTest, EmptyString) {
+	std::string test_str = "";
+	EXPECT_EQ(0, count(test_str));
+}
+
+TEST(CountTest, ManySpaces) {
+	std::string test_str = "   this   string has     weird   spacing";
+	EXPECT_EQ(5, count(test_str));
 }
 
 int main(int argc, char **argv) {
